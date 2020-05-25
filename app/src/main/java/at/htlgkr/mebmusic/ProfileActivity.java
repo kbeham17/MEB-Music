b25a00293380b5ffd3ed696344d7ac723cc39135
kbeham17 <kevinbeham2@gmail.com>
2020-05-25 16:36:15 +0200

    Google Sign In Step 1

--- app/src/main/java/at/htlgkr/mebmusic/ProfileActivity.java
+++ app/src/main/java/at/htlgkr/mebmusic/ProfileActivity.java
@@ -2,16 +2,13 @@ package at.htlgkr.mebmusic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, StartActivity.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_profile);
     }
 }
---
+++ app/src/main/java/at/htlgkr/mebmusic/StartActivity.java
@@ -0,0 +1,67 @@
+package at.htlgkr.mebmusic;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class StartActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+
+    SignInButton signInButton;
+    private GoogleApiClient googleApiClient;
+    private static final int SIGN_IN = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startpage);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        googleApiClient = new GoogleApiClient
+                .Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        signInButton = findViewById(R.id.googleSignIn);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                startActivityForResult(intent, SIGN_IN);
+            }
+        });
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == SIGN_IN){
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+
+            if(result.isSuccess()){
+                startActivity(new Intent(StartActivity.this, ProfileActivity.class));
+                finish();
+            }
+        }
+    }
+}
