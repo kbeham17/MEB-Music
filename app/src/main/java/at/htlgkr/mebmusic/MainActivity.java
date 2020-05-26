fd2d93de3f9d32bbe10fce9b5b5ac3aa68655900
kbeham17 <kevinbeham2@gmail.com>
2020-05-26 18:02:07 +0200

    Logout

--- app/src/main/java/at/htlgkr/mebmusic/MainActivity.java
+++ app/src/main/java/at/htlgkr/mebmusic/MainActivity.java
@@ -2,11 +2,17 @@ package at.htlgkr.mebmusic;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -14,6 +20,14 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import at.htlgkr.mebmusic.fragment.LogoutFragment;
+import at.htlgkr.mebmusic.fragment.PlaylistFragment;
+import at.htlgkr.mebmusic.fragment.ProfileFragment;
+import at.htlgkr.mebmusic.fragment.SearchFragment;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
 
@@ -23,16 +37,23 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     private String email;
     private String id;
 
-    private GoogleApiClient googleApiClient;
+    private LogoutFragment logoutFragment = new LogoutFragment();
+    private PlaylistFragment playlistFragment = new PlaylistFragment();
+    private ProfileFragment profileFragment = new ProfileFragment();
+    private SearchFragment searchFragment = new SearchFragment();
+    private BottomNavigationView menuBottomNavigationView;
+
+    public static GoogleApiClient googleApiClient;
     private GoogleSignInOptions gso;
+
+    private Button buttonLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, StartActivity.class);
-        startActivityForResult(intent, RQ_RESULT_START_ACTIVITY);
+        gotoStartActivity();
 
         gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
@@ -43,6 +64,59 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
 
+        menuBottomNavigationView = findViewById(R.id.menu_bottomNavigationView);
+
+        setFragment(profileFragment);
+        menuBottomNavigationView.setSelectedItemId(R.id.menu_profile);
+        menuBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                if (menuItem.isChecked()){
+                    return true;
+                } else {
+                    switch (menuItem.getItemId()){
+                        case R.id.menu_profile:
+                            profileFragment = new ProfileFragment();
+                            setFragment(profileFragment);
+                            return true;
+                        case R.id.menu_playlist:
+                            playlistFragment = new PlaylistFragment();
+                            setFragment(playlistFragment);
+                            return true;
+                        case R.id.menu_search:
+                            searchFragment = new SearchFragment();
+                            setFragment(searchFragment);
+                            return true;
+                        case R.id.menu_logout:
+                            profileFragment = new ProfileFragment();
+                            setFragment(profileFragment);
+                                    Auth.GoogleSignInApi.signOut(googleApiClient).setResultCallback(new ResultCallback<Status>() {
+                                        @Override
+                                        public void onResult(@NonNull Status status) {
+                                            if(status.isSuccess()){
+                                                gotoStartActivity();
+                                            } else {
+                                                Toast.makeText(MainActivity.this,"Login Failed!", Toast.LENGTH_LONG).show();
+                                            }
+                                        }
+                                    });
+
+                            return true;
+                        default:
+                            setFragment(profileFragment);
+                            return true;
+                    }
+
+                }
+            }
+        });
+
+
+    }
+
+    private void gotoStartActivity() {
+        Intent intent = new Intent(this, StartActivity.class);
+        startActivityForResult(intent, RQ_RESULT_START_ACTIVITY);
     }
 
     @Override
@@ -59,5 +133,11 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
             email = account.getEmail();
             id = account.getId();
         }
+    }
+
+    private void setFragment(Fragment fragment){
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.frame_main, fragment);
+        ft.commit();
     }
 }
